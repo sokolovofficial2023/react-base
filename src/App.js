d81cfbae889450b4736ec1d92d53a9832d3edf05
sokolovofficial2023 <dev@tararoutray.com>
2023-09-18 20:58:12 +0300

    add-custom-hooks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, {useMemo, useRef, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/myModal/MyModal";
+import {usePost} from "./components/hooks/usePosts";
 
 function App() {
 
@@ -19,17 +18,11 @@ function App() {
             {id: 5, title: 'C++', body: 'Description'},
         ]
     )
-
-
     const [filter, setFilter] = useState({sort: '', query: ''})
-
     const [modal, setModal] = useState(false)
+    const sortedAndSearchPosts = usePost(posts, filter.sort, filter.query)
 
 
-
-    const sortedAndSearchPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.includes(filter.query))
-    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -40,23 +33,21 @@ function App() {
         setPosts(posts.filter(post => post.id !== remPost.id))
     }
 
-
     return (
         <div className="App">
             <MyButton
                 style={{marginTop:'30px'}}
                 onClick={()=>setModal(!modal)}
-
             >
                 Создать пост
             </MyButton>
+
             <MyModal
                 visible={modal}
                 setVisible={setModal}
             >
                 <PostForm create={createPost}/>
             </MyModal>
-
 
             <PostFilter
                 filter={filter}
--- src/components/hooks/usePosts.jsx
+++ src/components/hooks/usePosts.jsx
@@ -7,5 +7,17 @@ export const useSortedPosts = (posts,sort) => {
         }
         return posts
     }, [sort, posts])
+
+    return sortedPosts
+}
+
+export const usePost = (posts, sort, query) => {
+    const sortedPosts = useSortedPosts(posts, sort)
+
+    const sortedAndSearchPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.includes(query))
+    }, [query, sortedPosts])
+
+    return sortedAndSearchPosts;
 }
 
